3cad4af935e78694a94955ffafa3dfd546b2bb84
Catalin Ionescu <catalin.tarhon@gmail.com>
2022-02-13 00:08:18 +0200

    contact section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+// Hamburger Show
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('show');
+    navUl.classList.toggle('show');
+})
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('show');
+    navUl.classList.remove('show');
+}))
 
 
 
